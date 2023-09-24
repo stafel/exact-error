683bf26e5842c60218ff4d760d8fccb000eaa32c
Rafael Stauffer <rafael.staffer@hotmail.de>
2023-09-24 16:13:10 +0200

    feat: calculate rounded float with error

--- exact-error.go
+++ exact-error.go
@@ -1,16 +1,33 @@
 package main
 
 import (
+	"fmt"
 	"encoding/csv"
 	"log"
 	"os"
+	"math"
 )
+
+func roundFloat(val float64, precision uint) float64 {
+    ratio := math.Pow(10, float64(precision))
+    return math.Round(val*ratio) / ratio
+}
+
 
 func main() {
 	data := [][]string{
-		{"a", "1"},
-		{"b", "2"},
-		{"c", "3"},
+		{"Input", "Output", "Error"},
+	}
+
+	for i:=1.0; i<=7.0; i+=0.00001 {
+		input := roundFloat(i, 5)
+		output := i / 6.0 * 100.0
+		output = output / 100.0 * 6.0
+		output = roundFloat(output, 5)
+		diffError := input - output
+
+		newSlice := []string{fmt.Sprintf("%v", input), fmt.Sprintf("%v", output), fmt.Sprintf("%v", diffError)}
+		data = append(data, newSlice)
 	}
 
 	file, err := os.Create("output.csv")
