9fbfe47d36a32448ba1e1fa9d051fa40eff841b6
Rafael Stauffer <rafael.staffer@hotmail.de>
2023-09-24 16:27:42 +0200

    feat: automatic rounding to correct precision

--- exact-error.go
+++ exact-error.go
@@ -19,15 +19,35 @@ func main() {
 		{"Input", "Output", "Error"},
 	}
 
-	for i:=1.0; i<=7.0; i+=0.00001 {
-		input := roundFloat(i, 5)
-		output := i / 6.0 * 100.0
-		output = output / 100.0 * 6.0
-		output = roundFloat(output, 5)
-		diffError := input - output
+	for j:=10.0; j < 10000000; j = j * 10 {
 
-		newSlice := []string{fmt.Sprintf("%v", input), fmt.Sprintf("%v", output), fmt.Sprintf("%v", diffError)}
-		data = append(data, newSlice)
+		var stepSize float64
+		stepSize = 1.0 / j
+
+		roundingSize := uint(math.Log10(j)) // this is the precision, number of after comma numbers
+
+		stepSize = roundFloat(stepSize, roundingSize)
+
+		log.Print(fmt.Sprintf("Calculating grades with step size %v", stepSize))
+
+		for i:=1.0; i<=7.0; i+=stepSize {
+			input := i
+			input = roundFloat(input, roundingSize)
+
+			output := i / 6.0 * 100.0
+			output = output / 100.0 * 6.0
+			output = roundFloat(output, roundingSize)
+
+			diffError := input - output
+
+			if diffError != 0.0 {
+			input := roundFloat(i, 5)
+				log.Fatal(fmt.Sprintf("Found difference error at step size %v and rounding %v with input %v, output %v, error %v", stepSize, roundingSize, input, output, diffError))
+			}
+
+			newSlice := []string{fmt.Sprintf("%v", input), fmt.Sprintf("%v", output), fmt.Sprintf("%v", diffError)}
+			data = append(data, newSlice)
+		}
 	}
 
 	file, err := os.Create("output.csv")
